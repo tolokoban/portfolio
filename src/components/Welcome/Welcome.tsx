b9a15c8e5130713472a38116a570fdac37a8d470
Tolokoban <contact@tolokoban.org>
2023-04-28 18:00:21 +0200

    Tromnine plus trisecta

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -5,9 +5,125 @@ export interface WelcomeProps {
 }
 
 export default function Welcome({ className }: WelcomeProps) {
+    const diskColor = "#204657"
     return (
         <div className={join(className, Style.Welcome)}>
-            <ul>
+            <header>Tolokoban</header>
+            <main>
+                <svg
+                    version="1.1"
+                    preserveAspectRatio="xMidYMid meet"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="-1100 -1100 2200 2200"
+                >
+                    <defs>
+                        <linearGradient
+                            id="grad"
+                            gradientTransform="rotate(-30)"
+                        >
+                            <stop offset="0%" stop-color={diskColor} />
+                            <stop offset="25%" stop-color={diskColor} />
+                            <stop offset="100%" stop-color="#f90" />
+                        </linearGradient>
+                    </defs>
+
+                    <g fill="#0003" stroke="none" stroke-width="3">
+                        <path
+                            d="M0,0 Q-500,-500,0,-1000 A1000,1000 0,0,0 -866,-500"
+                            transform="rotate(0) translate(-43.3,25)"
+                            fill="url(#grad)"
+                        />
+                        <path
+                            d="M0,0 Q-500,-500,0,-1000 A1000,1000 0,0,0 -866,-500"
+                            transform="rotate(120) translate(-43.3,25)"
+                            fill="url(#grad)"
+                        />
+                        <path
+                            d="M0,0 Q-500,-500,0,-1000 A1000,1000 0,0,0 -866,-500"
+                            transform="rotate(240) translate(-43.3,25)"
+                            fill="url(#grad)"
+                        />
+
+                        <path
+                            d="M-866,-500 A1000,1000 0,0,0 -866,500 Q-183,683,0,0"
+                            transform="rotate(0) translate(-43.3,25)"
+                            fill={diskColor}
+                        />
+                        <path
+                            d="M-866,-500 L0,0"
+                            stroke-width={10}
+                            transform="rotate(0) translate(-43.3,25)"
+                            stroke={diskColor}
+                        />
+                        <path
+                            d="M-866,-500 A1000,1000 0,0,0 -866,500 Q-183,683,0,0"
+                            transform="rotate(120) translate(-43.3,25)"
+                            fill={diskColor}
+                        />
+                        <path
+                            d="M-866,-500 L0,0"
+                            stroke-width={10}
+                            transform="rotate(120) translate(-43.3,25)"
+                            stroke={diskColor}
+                        />
+                        <path
+                            d="M-866,-500 A1000,1000 0,0,0 -866,500 Q-183,683,0,0"
+                            transform="rotate(240) translate(-43.3,25)"
+                            fill={diskColor}
+                        />
+                        <path
+                            d="M-866,-500 L0,0"
+                            stroke-width={10}
+                            transform="rotate(240) translate(-43.3,25)"
+                            stroke={diskColor}
+                        />
+                    </g>
+
+                    <g
+                        style={{
+                            fontSize: "150px",
+                        }}
+                        fill="#f90"
+                    >
+                        <path
+                            id="ARC"
+                            d="M-800,0A800,800,0 0 0 0,800"
+                            transform="rotate(30)"
+                            stroke="none"
+                            fill="none"
+                        />
+                        <path
+                            id="ARC2"
+                            d="M-800,0A800,800,0 0 1 0,-800"
+                            transform="rotate(-30)"
+                            stroke="none"
+                            fill="none"
+                        />
+                        <text
+                            width="1000"
+                            transform="rotate(0)"
+                            alignment-baseline="middle"
+                        >
+                            <textPath href="#ARC2">Créations</textPath>
+                        </text>
+                        <text
+                            width="1000"
+                            transform="rotate(120)"
+                            alignment-baseline="middle"
+                        >
+                            <textPath href="#ARC2">Blog</textPath>
+                        </text>
+                        <text
+                            width="1000"
+                            transform="rotate(240)"
+                            alignment-baseline="middle"
+                        >
+                            <textPath href="#ARC">Cursus</textPath>
+                        </text>
+                    </g>
+                </svg>{" "}
+            </main>
+            {/* <ul>
                 <li>
                     <a href="#/Minervois">Minervois</a>
                 </li>
@@ -20,7 +136,7 @@ export default function Welcome({ className }: WelcomeProps) {
                 <li>
                     <a href="#/article/webgl/Blending">WebGL Blending</a>
                 </li>
-            </ul>
+            </ul> */}
             <img className={Style.trombine} src="images/trombine.mini.webp" />
         </div>
     )
