d566f1495a1ff1897d25a492f581f31e024fd9f3
Tolokoban <contact@tolokoban.org>
2024-11-03 12:24:38 +0100

    Add info in CV

--- src/app/cv/layout.tsx
+++ src/app/cv/layout.tsx
@@ -1,60 +1,60 @@
-import React from "react"
+import React from "react";
 
-import { IconBrandGithub, IconMail } from "@/ui"
-import { useLangValue } from "@/hooks/lang"
+import { IconBrandGithub, IconMail } from "@/ui";
+import { useLangValue } from "@/hooks/lang";
 
-import styles from "./layout.module.css"
+import styles from "./layout.module.css";
 
 export default function LayoutCV({ children }: { children: React.ReactNode }) {
-    const lang = useLangValue()
-    const tr = TRANSLATIONS[lang] ?? EN
-    const handleEMailAddress = (a: HTMLAnchorElement | null) => {
-        if (!a) return
+  const lang = useLangValue();
+  const tr = TRANSLATIONS[lang] ?? EN;
+  const handleEMailAddress = (a: HTMLAnchorElement | null) => {
+    if (!a) return;
 
-        a.setAttribute("href", "mailto:" + ["info", "tolokoban.org"].join("@"))
-    }
-    return (
-        <div className={styles.main}>
-            <div>
-                <header>
-                    <div>Fabien PETITJEAN</div>
-                    <a href="mailto:antispam" ref={handleEMailAddress}>
-                        <IconMail />
-                        <div>{tr.email}</div>
-                    </a>
-                    <a href="https://github.com/tolokoban">
-                        <IconBrandGithub />
-                        <div>GitHub</div>
-                    </a>
-                </header>
-                <h1>
-                    Developer Fullstack
-                    <br />
-                    Visualization engineer
-                    <br />
-                    <small>
-                        <a href="#/">( Look at my portfolio )</a>
-                    </small>
-                </h1>
-                {children}
-                <hr />
-                <p>
-                    <a href="#/">Back to portfolio</a>.
-                </p>
-            </div>
-        </div>
-    )
+    a.setAttribute("href", "mailto:" + ["info", "tolokoban.org"].join("@"));
+  };
+  return (
+    <div className={styles.main}>
+      <div>
+        <header>
+          <div>Fabien PETITJEAN</div>
+          <a href="mailto:antispam" ref={handleEMailAddress}>
+            <IconMail />
+            <div>{tr.email}</div>
+          </a>
+          <a href="https://github.com/tolokoban">
+            <IconBrandGithub />
+            <div>GitHub</div>
+          </a>
+        </header>
+        <h1>
+          Fullstack Developer
+          <br />
+          Visualization Engineer
+          <br />
+          <small>
+            <a href="#/">( Look at my portfolio )</a>
+          </small>
+        </h1>
+        {children}
+        <hr />
+        <p>
+          <a href="#/">Back to portfolio</a>.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 const FR = {
-    email: "Addresse mail",
-}
+  email: "Addresse mail",
+};
 
 const EN: typeof FR = {
-    email: "EMail address",
-}
+  email: "EMail address",
+};
 
 const TRANSLATIONS: Record<string, typeof FR> = {
-    fr: FR,
-    en: EN,
-}
+  fr: FR,
+  en: EN,
+};
